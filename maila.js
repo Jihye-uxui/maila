aaf1aa7cb688e66b6f58b79c766a27e587acdfc4
Jihye Choe <choijihye37@gmail.com>
2020-11-21 13:14:29 -0800

    Responsive Nav-bar

--- maila.js
+++ maila.js
@@ -27,3 +27,8 @@ window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+window.addEventListener("scroll", function(){
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("sticky", window.scrollY > 0);
+})
+
